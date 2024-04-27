2a76a5187e6c58c4a6476421cec0a9ad65062974
Sidharth Kumar <sidharthkumar@Sidharths-MacBook-Pro.local>
2024-04-28 01:17:45 +0530

    Added more info to products card

--- frontend/components/Main/Product.js
+++ frontend/components/Main/Product.js
@@ -6,6 +6,8 @@ const Product = ({product}) => {
       <img src={product.thumbnail} className='md:w-[400px] md:h-[300px] h-[200px] w-[200px] rounded-xl group-hover:scale-105 transition-all ease-out'></img>
       <div className='flex flex-col justify-center items-center gap-y-2 md:text-xl text-xs md:font-extrabold md:mt-5 mt-2'>
         <h1>{product.title}</h1>
+        <h1 >₹{product.price}</h1>
+        <h2 className='text-gray-800 font-light line-through'>₹{parseInt(parseInt(product.price) / (1 - (product.discountPercentage/100)))}</h2>
         <h1 className='text-green-800'>Discount : {product.discountPercentage}%</h1>
       </div>
       </div>
--- frontend/components/Main/TopSelling.js
+++ frontend/components/Main/TopSelling.js
@@ -24,7 +24,7 @@ const TopSelling = () => {
         <h1 className='text-green-800 text-4xl font-extrabold max-sm:text-center pb-4'>Heavily Discounted Products</h1>
         <div className='grid grid-cols-12 mt-4 gap-4'>
             {products?.map((product,index) => {
-                return <div className='flex flex-col justify-end col-span-6 md:col-span-3 md:p-10 p-4 group border'> 
+                return <div key={index} className='flex flex-col justify-end col-span-6 md:col-span-3 md:p-10 p-4 group border'> 
                         <Product product={product}></Product>
                     </div>
             })}
